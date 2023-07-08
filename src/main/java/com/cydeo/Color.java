33a57e8303682f3345f66d6f23b0b6ec512a274f
efecagrisen <efecagrisen@yahoo.com>
2023-07-08 14:10:24 +0300

    task 1

---
+++ src/main/java/com/cydeo/Color.java
@@ -0,0 +1,7 @@
+package com.cydeo;
+
+public enum Color {
+
+    ORANGE,RED,GREEN
+
+}
---
+++ src/main/java/com/cydeo/OrangeTest.java
@@ -0,0 +1,22 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrangeTest {
+
+    public static void main(String[] args) {
+
+        List<Orange> inventory = new ArrayList<>();
+        inventory.add(new Orange(300,Color.GREEN));
+        inventory.add(new Orange(200,Color.RED));
+        inventory.add(new Orange(150,Color.ORANGE));
+        inventory.add(new Orange(80,Color.ORANGE));
+
+
+
+    }
+
+
+
+}
